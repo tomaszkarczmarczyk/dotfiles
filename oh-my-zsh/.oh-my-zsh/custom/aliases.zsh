88c5e438fd2ca14a2f42a11f30d7602cf971170b
Tomasz Karczmarczyk <tokarczmarczyk@gmail.com>
2026-04-10 17:41:15 +0200

    Add Oh My Zsh

---
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,9 @@
+alias c="clear"
+
+alias eza='eza -lhoATL 1 --time-style relative --group-directories-first --icons=always'
+
+alias refresh="source ~/.zshrc"
+
+alias zshconfig="nvim ~/.zshrc"
+alias nvimconfig="nvim ~/.config/nvim/init.lua"
+
