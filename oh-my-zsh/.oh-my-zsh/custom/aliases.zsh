cacd93780de721b21a2fa82ac149de63bd07ede4
Tomasz Karczmarczyk <tokarczmarczyk@gmail.com>
2026-08-24 10:51:44 +0200

    Update Oh My Zsh

--- oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,17 +1,19 @@
-alias refresh='source ~/.zshrc'
+alias reload="source $HOME/.zshrc && echo 'zsh config reloaded!'"
 
 alias c=clear
 alias history='omz_history -E'
 
-alias eza='eza -lhgoATL 1 --time-style relative --group-directories-first --icons=always'
+alias eza='eza -lhgATL 1 --time-style relative --group-directories-first --icons=always'
 alias fd='fd --color=always'
 alias stow='stow -v'
 
-alias config-zsh='nvim ~/.zshrc'
-alias config-aliases='nvim ~/.oh-my-zsh/custom/aliases.zsh'
-alias config-nvim='nvim ~/.config/nvim/init.lua'
+alias g=git
+
+alias config-aliases="$EDITOR $HOME/.oh-my-zsh/custom/aliases.zsh"
+alias config-nvim="$EDITOR $HOME/.config/nvim/init.lua"
+alias config-zsh="$EDITOR $HOME/.zshrc"
 
 alias buuc='brew update && brew upgrade && brew cleanup'
 
-alias icloud='z ~/Library/Mobile\ Documents/com~apple~CloudDocs'
-alias obsidian='z ~/library/Mobile\ Documents/iCloud~md~obsidian/documents'
+alias icloud="z $HOME/Library/Mobile\ Documents/com~apple~CloudDocs"
+alias obsidian="z $HOME/library/Mobile\ Documents/iCloud~md~obsidian/documents"
